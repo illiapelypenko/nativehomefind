732428c06ca515f1054aefdd1033310414b4e461
IlliaPelypenko <pelypenkoillya@gmail.com>
2020-09-23 11:34:52 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return <h1>Hello</h1>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ ios/Pods/Target Support Files/Pods-nativehomefind-nativehomefindTests/Pods-nativehomefind-nativehomefindTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nativehomefind_nativehomefindTests : NSObject
+@end
+@implementation PodsDummy_Pods_nativehomefind_nativehomefindTests
+@end
